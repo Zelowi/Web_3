dedb2939c51b26755c6c4a92de6abdce17c62bbf
Zelowi <153396306+Zelowi@users.noreply.github.com>
2024-06-08 23:35:48 +0300

    Add files via upload

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<!-- App.vue -->
+<template>
+    <div id="app">
+      <router-view />
+    </div>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style>
+  @import "@/assets/style.css";
+  
+  #app {
+    font-family: Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
